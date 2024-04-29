99cf3bcd41bd726cfe38348de89fa2ecd61559be
nkweku66 <nkweku66@gmail.com>
2024-04-29 15:53:31 +0000

    README: created

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo constains basic python projects
---
+++ python_projects/madlibs.py
@@ -0,0 +1,44 @@
+# Madlibs python game
+# Author - Nana Obeng
+# Document used - from https://www.madlibs.com/printables/
+# Date - 04/29/2024
+
+# introduction
+print("Welcome to MADlibs!!\n")
+print("Hello! " + input("What is your name: "))
+print("Help me generate a wonderful story. Provide me the following: \n")
+
+# variables
+adj1 = input("Type an adjective: ")
+adj2 = input("Type a second adjective: ")
+adj3 = input("Type a third adjective: ")
+
+noun1 = input("Type a noun: ")
+noun2 = input("Type a second noun: ")
+noun3 = input("Type a third noun: ")
+
+plu_noun1 = input("Type a plural noun: ")
+plu_noun2 = input("Type a second plural noun: ")
+plu_noun3 = input("Type a third plural noun: ")
+plu_noun4 = input("Type a fourth plural noun: ")
+
+verb1 = input("Type a verb: ")
+verb2 = input("Type a second verb: ")
+verb3 = input("Type a third verb: ")
+verb4 = input("Type your last verb: ")
+
+game = input("Type a name of a game: ")
+plant = input("Type the name of a plant: ")
+part_of_body = input("Type the name of a body part: ")
+num = input("Type a number: ")
+place = input("Type a place: ")
+
+# story
+print("A vacation is when a trip to some " + adj1 + " place with your " + adj2 + " family.")
+print(" Usually you go to some place that is near a/an " + noun1 + " or up on a/an " + noun2 + ".")
+print("A good vacation place is one where you can ride " + plu_noun1 + "or play " + game + " or go hunting for " + plu_noun2 + ".")
+print("I like to spend my time " + verb1 + " or " + verb2 + ".")
+print("When parents go on a vacation, they spend their time eating three " + plu_noun3 + "  a day, and fathers play golf, and mothers sit around " + verb3 + ".")
+print(" Last summer, my little brother VERB ENDING IN “ING” fell in a/an " + noun3 + " and got poison " + plant + " all over his " + part_of_body + ".")
+print("My family is going to go to (the) " + place + ", and i will practice " + verb4 + ".")
+print("Parents need vacations more than kids because parents are always very " + adj3 + " and because they work " + num + " hours every day all year making enough " + plu_noun4 + " to pay for the vacation.")
